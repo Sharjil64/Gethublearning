9e752b15d5a7ac7b653aa232e24bcd20e890fd09
Sharjil64 <sharjeelahmed246810@gmail.com>
2025-02-24 00:56:59 +0500

    App.js file update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/NavBar";
+import Education from "./component/Education";
+import Port from "./Port/Port";
 
 function App() {
   return (
-  <div className="Nabar">
-    Hello
-  </div>
+    <Router>
+      <Navbar />
+      <Education />
+      <Routes>
+        <Route path="/port" element={<Port />} />
+        <Route path="/port" element={<Navbar />} />
+        <Route path="/port" element={<Education />} />
+      </Routes>
+    </Router>
   );
 }
 
